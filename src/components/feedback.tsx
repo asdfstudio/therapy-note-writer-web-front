e8a8284a56016c0ea66819864b192431c06990a0
Meraj Kazi <kazimeraj11@gmail.com>
2023-08-30 19:03:39 +0600

    feedback section

--- src/components/feedback.tsx
+++ src/components/feedback.tsx
@@ -1,28 +1,63 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const feedbacks = [
+const slides = [
   {
-    key: 1,
     desc: 'Saved 10 hours on week 1. No more busy work for me. Only patient care. Just love the service. Easy, fast and very convenient.',
     name: 'Dr. Adam Voigt',
     subTitle: 'PhD in Mental Health',
   },
   {
-    key: 1,
-    desc: 'Everything is just better with this tool. Highly suggested. I wish I had known about this earlier',
+    desc: 'Everything is just better with this tool. Highly suggested. I wish I had known about this earlier.',
+    name: 'Dr. Jordan Franklin',
+    subTitle: 'PhD in Mental Health',
+  },
+  {
+    desc: 'Saved 10 hours on week 1. No more busy work for me. Only patient care. Just love the service. Easy, fast and very convenient.',
+    name: 'Dr. Adam Voigt',
+    subTitle: 'PhD in Mental Health',
+  },
+  {
+    desc: 'Everything is just better with this tool. Highly suggested. I wish I had known about this earlier.',
     name: 'Dr. Jordan Franklin',
     subTitle: 'PhD in Mental Health',
   },
 ];
 
 const Feedback = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const autoChange = () => {
+      const isLastSlide = currentIndex === slides.length - 1;
+      const newIndex = isLastSlide ? 0 : currentIndex + 1;
+      setCurrentIndex(newIndex);
+    };
+    const interval = setInterval(() => {
+      autoChange();
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
   return (
     <div className='bg-[#F0F4FF] flex flex-col items-center'>
       {/* Title */}
       <div
-        className='w-[23.6rem] mb-[2.31rem] mt-[1.81rem]
+        className='w-[23.6rem] mb-[3.61rem] mt-[1.81rem]
           sm:w-[29.5rem]'
       >
         <h2
@@ -47,54 +82,112 @@ const Feedback = () => {
         />
       </div>
       {/* Cards */}
-      <div>
+      <div className='flex flex-col relative items-center'>
+        <Image
+          src={'/icon-double-quote.svg'}
+          alt=''
+          width={1200}
+          height={550}
+          className='absolute w-[5.175rem] h-[3.5625rem]
+          top-[-1.8rem]'
+        />
         {/* Card Body */}
-        <div className='bg-[#fff] w-[21.25rem] h-[21.87rem]'>
-          {/* Left Arrow */}
-          <Image
-            src={'/'}
-            alt=''
-            width={1200}
-            height={550}
-            className='hidden'
-          />
+        <div
+          className='bg-[#fff] w-[21.25rem] h-[21.87rem]
+          flex flex-col justify-center items-center'
+        >
           {/* Text Contents */}
-          {feedbacks.map((item, index) => {
-            return (
-              <div key={index}>
-                <h3>{item.desc}</h3>
-                {/* Stars */}
-                <div>
-                  <Image
-                    src={'/icon-stars.svg'}
-                    alt=''
-                    width={1200}
-                    height={550}
-                    className='w-[11.875rem]'
-                  />
-                </div>
-                {/* Name */}
-                <h3></h3>
-                {/* Sub-title */}
-                {/* h3
-                 */}
-              </div>
-            );
-          })}
-          {/* Right Arrow */}
+          <h3
+            className='w-[20rem] font-iBM_Plex_Sans 
+            text-[1.375rem] font-[400] text-center mb-2'
+          >
+            {slides[currentIndex].desc}
+          </h3>
+          {/* Stars */}
           <Image
-            src={'/'}
+            src={'/icon-stars.svg'}
             alt=''
             width={1200}
             height={550}
-            className='hidden'
+            className='w-[11.875rem] mb-[0.31rem]'
+          />
+          {/* Name */}
+          <h3
+            className='font-iBM_Plex_Sans font-[700] 
+            text-[1.5rem] text-[#29375F]'
+          >
+            {slides[currentIndex].name}
+          </h3>
+          {/* Sub-title */}
+          <h3
+            className='font-iBM_Plex_Sans font-[400] 
+            text-[1.125rem] text-[#29375F]'
+          >
+            {slides[currentIndex].subTitle}
+          </h3>
+        </div>
+        {/* Arrow */}
+        <div className='flex justify-center mt-4 gap-2'>
+          {/* Left Arrow */}
+          <div
+            onClick={prevSlide}
+            className='flex bg-[#6F91F4] w-[2.75rem]
+            h-[2.75rem] rounded-full justify-center
+            border-[1px] shadow-[#3b60cb40] shadow-md'
+          >
+            <Image
+              src={'/icon-arrow-left.svg'}
+              alt=''
+              width={1200}
+              height={550}
+              className='w-[1rem]'
+            />
+          </div>
+          {/* Right Arrow */}
+          <div
+            onClick={nextSlide}
+            className='flex bg-[#6F91F4] w-[2.75rem]
+            h-[2.75rem] rounded-full justify-center
+            border-[1px] shadow-[#3b60cb40] shadow-md'
+          >
+            <Image
+              src={'/icon-arrow-right-2.svg'}
+              alt=''
+              width={1200}
+              height={550}
+              className='w-[1rem]'
+            />
+          </div>
+        </div>
+        {/* Slider bar */}
+        <div
+          className='flex mt-4 rounded-full w-[8.3125rem] 
+          h-[0.75rem] bg-[#a4b8f4]'
+        >
+          <hr
+            className={`w-[2.1875rem] h-[0.75rem] ${
+              currentIndex === 0 ? `bg-[#6F91F4] opacity-100` : `opacity-0`
+            } rounded-full`}
+          />
+          <hr
+            className={`w-[2.1875rem] h-[0.75rem] ${
+              currentIndex === 1 ? `bg-[#6F91F4] opacity-100` : `opacity-0`
+            } rounded-full`}
+          />
+          <hr
+            className={`w-[2.1875rem] h-[0.75rem] ${
+              currentIndex === 2 ? `bg-[#6F91F4] opacity-100` : `opacity-0`
+            } rounded-full`}
+          />
+          <hr
+            className={`w-[2.1875rem] h-[0.75rem] ${
+              currentIndex === 3 ? `bg-[#6F91F4] opacity-100` : `opacity-0`
+            } rounded-full`}
           />
         </div>
-        {/* Slider pointer */}
-        <div></div>
       </div>
       {/* Button */}
-      <Link href='/' className='w-[17rem] mt-10'>
+      <Link href='/' className='w-[17rem] mt-6'>
         <button
           className='flex bg-[#6F91F4] py-[12px] 
             w-full rounded-full border-[1px] 
