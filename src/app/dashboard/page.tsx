d9a8b776bcb086665abe1e71de723f2b0518e7c6
Meraj Kazi <kazimeraj11@gmail.com>
2023-09-23 14:35:08 +0600

    Dashboard setup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,105 @@
 'use client';
-import Navbar2 from '@/components/Navbar2';
-import Link from 'next/link';
-import React from 'react';
+import Menubody from '@/components/menubody';
+import SecondSection from '@/components/secondSection';
+import Image from 'next/image';
+import React, { useState } from 'react';
 
-const dashboardPage = () => {
+const DashboardPage = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className=''>
-      <div>Dashboard</div>
+      {/* Logo and Menu */}
+      <div
+        className='fixed h-[4.75rem] w-screen bg-[#12192E] font-iBM_Plex_Sans
+        text-[#fff] flex justify-center flex-col'
+      >
+        {/* Menu Bar Items */}
+        <div className='flex'>
+          {/* Logo and menu bar */}
+          <div className='flex'>
+            <Image
+              src={'/dashboard-logo-1.svg'}
+              alt=''
+              width={1200}
+              height={550}
+              className='w-14'
+            />
+            <Image
+              src={'/dashboard-logo-2.svg'}
+              alt=''
+              width={1200}
+              height={550}
+              className='w-20'
+            />
+          </div>
+          <div className='flex'>
+            <div
+              onClick={() => {
+                !showMenu ? setShowMenu(true) : setShowMenu(false);
+              }}
+            >
+              <Image
+                src={'/dashboard-icon-pen.svg'}
+                alt=''
+                width={1200}
+                height={550}
+                className='w-10'
+              />
+            </div>
+            <div>
+              <Image
+                src={'/dashboard-icon-user.svg'}
+                alt=''
+                width={1200}
+                height={550}
+                className='w-10'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Menu body */}
+      <div className={!showMenu ? 'hidden' : ''}>
+        <div
+          className='bg-[#12192E] w-screen h-screen text-[#fff] font-iBM_Plex_Sans
+          fixed flex flex-col top-[4.75rem]'
+        >
+          <div>
+            <p className='font-[700]'>Session Description</p>
+            <p className=' opacity-[0.6]'>Pronouns</p>
+            <div>
+              <input
+                type='radio'
+                value='She/Her/Hers'
+                id='option_1'
+                name='pronouns'
+              />
+              <label htmlFor='option_1'>She/Her/Hers</label>
+              <input
+                type='radio'
+                value='He/Him/His'
+                id='option_2'
+                name='pronouns'
+              />
+              <label htmlFor='option_1'>He/Him/His</label>
+              <input
+                type='radio'
+                value='They/Them/Theirs'
+                id='option_3'
+                name='pronouns'
+              />
+              <label htmlFor='option_1'>They/Them/Theirs</label>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Body */}
+      <div className='pt-[calc(100vh-37rem)]'>
+        <SecondSection />
+      </div>
     </div>
   );
 };
 
-export default dashboardPage;
+export default DashboardPage;
