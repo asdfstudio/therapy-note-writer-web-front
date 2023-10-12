a2706d9a3f68d7509e00a0bb1a780dfc92717015
Meraj Kazi <kazimeraj11@gmail.com>
2023-10-12 10:12:42 +0600

    Context added in project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,38 +2,41 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { IBM_Plex_Sans } from 'next/font/google';
 import localFont from 'next/font/local';
+import { ContextProvider } from '../context/Context';
 
 const iBM_Plex_Sans = IBM_Plex_Sans({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-iBM_Plex_Sans',
+    subsets: ['latin'],
+    weight: ['400', '500', '600', '700'],
+    variable: '--font-iBM_Plex_Sans',
 });
 
 const poynter_Oldstyle_Display = localFont({
-  src: '../../public/font/PoynterOldstyleDisplay_Roman.ttf',
-  variable: '--font-poynter_Oldstyle_Display',
+    src: '../../public/font/PoynterOldstyleDisplay_Roman.ttf',
+    variable: '--font-poynter_Oldstyle_Display',
 });
 
 export const metadata: Metadata = {
-  title: 'Therapy Note Writer',
-  description: 'An AI-Powered Note taking App',
+    title: 'Therapy Note Writer',
+    description: 'An AI-Powered Note taking App',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang='en' className='scroll-smooth'>
-      <body>
-        <div
-          className={`${iBM_Plex_Sans.variable} ${poynter_Oldstyle_Display.variable}`}
-        >
-          {/* <Navbar /> */}
-          {children}
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en' className='scroll-smooth'>
+            <body>
+                <ContextProvider>
+                    <div
+                        className={`${iBM_Plex_Sans.variable} ${poynter_Oldstyle_Display.variable}`}
+                    >
+                        {/* <Navbar /> */}
+                        {children}
+                    </div>
+                </ContextProvider>
+            </body>
+        </html>
+    );
 }
