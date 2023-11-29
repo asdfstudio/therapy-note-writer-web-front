de5dea669574f1c6afb98bf7c418170d1ee59e7c
Meraj Kazi <kazimeraj11@gmail.com>
2023-11-29 21:19:24 +0600

    privacy policy, terms of service page and linkedin SSO config added

---
+++ src/app/privacypolicy/page.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import data from '../../../public/data.json';
+
+const PrivacyPolicyPage = () => {
+    return (
+        <div className='flex justify-center my-[1rem] mx-[2rem] text-justify'>
+            <div>
+                <p
+                    className='font-poynter_Oldstyle_Display font-[400]
+                        text-[3.5rem] text-[#29375F] leading-[3.85rem]
+                        mb-[0.75rem]'
+                >
+                    Privacy Policy
+                </p>
+                {/* paragraph */}
+                <p
+                    className='font-iBM_Plex_Sans font-[500]
+                        text-[1rem] text-[#6F91F4] leading-[1.75rem]
+                        mb-[1.5rem]'
+                >
+                    Last updated October 16, 2023 12:34 PM
+                </p>
+
+                {/* Terms of Service Card */}
+                <div className='flex'>
+                    <div
+                        className={` h-auto bg-[#fff] border-[rgba(111,145,244,0.50)] border-[1px]  
+                            rounded-[0.75rem] py-[0.75rem] px-[1.12rem]`}
+                    >
+                        {/* Title */}
+                        <p
+                            className='text-[1rem] text-[rgba(41,55,95,0.70)] font-[700]
+                            font-iBM_Plex_Sans mb-[1rem]'
+                        >
+                            PRIVACY POLICY FOR WEB
+                        </p>
+
+                        <div className=' '>
+                            <p
+                                className='text-[1rem] text-[rgba(41,55,95,0.70)] font-[400]
+                                 font-iBM_Plex_Sans '
+                            >
+                                {data[0].privacy}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PrivacyPolicyPage;
